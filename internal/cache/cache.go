1fe938f1bd8e47f5b6717cde9cb76a2b65718648
Suhas Aggarwal <suhasagg@gmail.com>
2022-09-10 02:29:07 +0530

    Update cache.go

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,6 +1,7 @@
 package cache
 
 // Cache represents an in-memory cache, regardless of cache policy.
+// Cache variants two queues (for least frequently as well as recently used) and ARC cache  
 type Cache interface {
 	Put(key []byte, value []byte)
 	Get(key []byte) []byte
